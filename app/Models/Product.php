9f0c73e97784a067be10171190965c383cf34e4a
romo19503-ctrl <romo19503@gmail.com>
2026-03-28 13:00:32 -0600

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,20 +13,23 @@ class Product extends Model
 
 
     protected $fillable = ['name', 'price', 'description', 'slug', 'image'];
-
-
     public function categories(): BelongsToMany
     {
         return $this->belongsToMany(Category::class);
     }
-
-
-
     protected static function boot()
     {
         parent::boot();
+
         static::creating(function ($product) {
             if (empty($product->slug)) {
+                $product->slug = Str::slug($product->name);
+            }
+        });
+
+
+        static::updating(function ($product) {
+            if ($product->isDirty('name')) {
                 $product->slug = Str::slug($product->name);
             }
         });
